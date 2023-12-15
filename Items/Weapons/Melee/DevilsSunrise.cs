a43811946e60e562a37b6ced811ef474b8407c58
Altixal <73398287+Altixal@users.noreply.github.com>
2023-12-15 10:28:04 +0700

    devil's sunrise is actually affected by melee damage minor rebalancing included (needs retest) there's some other archaic code here im not willing to look at

--- Items/Weapons/Melee/DevilsSunrise.cs
+++ Items/Weapons/Melee/DevilsSunrise.cs
@@ -12,7 +12,7 @@ namespace CalamityMod.Items.Weapons.Melee
     public class DevilsSunrise : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.Melee";
-        public static int BaseDamage = 480;
+        public static int BaseDamage = 420;
 
         public override void SetDefaults()
         {
@@ -42,8 +42,8 @@ namespace CalamityMod.Items.Weapons.Melee
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, ModContent.ProjectileType<DevilsSunriseProj>(), damage, knockback, player.whoAmI, 0f, 0f);
-            Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, ModContent.ProjectileType<DevilsSunriseCyclone>(), damage, knockback, player.whoAmI, 0f, 0f);
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<DevilsSunriseCyclone>(), damage, knockback, player.whoAmI);
             return false;
         }
 
