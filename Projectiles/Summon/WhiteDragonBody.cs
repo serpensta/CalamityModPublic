d82f43506e54b84f63092dc650436d1a744c65e4
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-01-06 13:33:52 -0500

    This thankfully seemed to be all that was needed

--- Projectiles/Summon/WhiteDragonBody.cs
+++ Projectiles/Summon/WhiteDragonBody.cs
@@ -26,8 +26,8 @@ namespace CalamityMod.Projectiles.Summon
             Projectile.friendly = true;
             Projectile.penetrate = -1;
             Projectile.timeLeft = 180;
-            Projectile.idStaticNPCHitCooldown = 6;
             Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 8;
             Projectile.tileCollide = false;
             Projectile.extraUpdates = 0;
             Projectile.aiStyle = -1;
