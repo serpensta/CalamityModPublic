c0b77b87c943bb4ef8870f1b93aa3e9955b0d267
GinYuH <72181395+GinYuH@users.noreply.github.com>
2024-01-06 22:52:18 -0800

    furnituer

---
+++ Items/Placeables/FurnitureAuric/AuricConsole.cs
@@ -0,0 +1,34 @@
+﻿using CalamityMod.Items.DraedonMisc;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using CalamityMod.Tiles.FurnitureAuric;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace CalamityMod.Items.Placeables.FurnitureAuric
+{
+    public class AuricConsole : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeables";
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<AuricConsoleTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<AuricPanel>(10).
+                AddIngredient<DraedonPowerCell>(8).
+                AddTile<CosmicAnvil>().
+                Register();
+        }
+    }
+}
---
+++ Items/Placeables/FurnitureAuric/AuricDisplay.cs
@@ -0,0 +1,34 @@
+﻿using CalamityMod.Items.DraedonMisc;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using CalamityMod.Tiles.FurnitureAuric;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace CalamityMod.Items.Placeables.FurnitureAuric
+{
+    public class AuricDisplay : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeables";
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<AuricDisplayTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<AuricPanel>(15).
+                AddIngredient<DraedonPowerCell>(5).
+                AddTile<CosmicAnvil>().
+                Register();
+        }
+    }
+}
