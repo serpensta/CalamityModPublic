2b19ea782e7ab2444d797db7f04842b2ef833426
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-01-26 11:56:52 -0500

    Depths Charm -> Iron Boots -> Anechoic Plating shimmer

--- Items/Accessories/AnechoicPlating.cs
+++ Items/Accessories/AnechoicPlating.cs
@@ -8,6 +8,10 @@ namespace CalamityMod.Items.Accessories
     public class AnechoicPlating : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Accessories";
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<DepthCharm>();
+        }
         public override void SetDefaults()
         {
             Item.width = 26;
