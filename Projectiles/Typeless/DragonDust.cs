243c52f6e0c2995a4107561b17026d48b5428a2a
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-02-04 16:19:00 -0500

    Blissful Bombardier balance
    Also dragon dust iframe change; 2 more global iframes killed
    Was originally gonna do Depth Crusher and Polaris Parrotfish, but they're both good

--- Projectiles/Typeless/DragonDust.cs
+++ Projectiles/Typeless/DragonDust.cs
@@ -18,6 +18,8 @@ namespace CalamityMod.Projectiles.Typeless
             Projectile.ignoreWater = true;
             Projectile.tileCollide = false;
             Projectile.penetrate = -1;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 3;
         }
 
         public override void AI()
@@ -80,11 +82,7 @@ namespace CalamityMod.Projectiles.Typeless
             }
         }
 
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            target.immune[Projectile.owner] = 0;
-            target.AddBuff(ModContent.BuffType<Dragonfire>(), 120);
-        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) => target.AddBuff(ModContent.BuffType<Dragonfire>(), 120);
 
         public override void OnHitPlayer(Player target, Player.HurtInfo info) => target.AddBuff(ModContent.BuffType<Dragonfire>(), 120);
     }
