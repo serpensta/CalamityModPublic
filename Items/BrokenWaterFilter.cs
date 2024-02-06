6e95a8a6523cd0f0466ec6949ed25b9740244916
Storm2103 <snowstorm2013@gmail.com>
2024-02-05 20:22:19 -0600

    the last 2

--- Items/BrokenWaterFilter.cs
+++ Items/BrokenWaterFilter.cs
@@ -2,11 +2,15 @@
 using Terraria.ID;
 using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 
 namespace CalamityMod.Items
 {
     public class BrokenWaterFilter : ModItem, ILocalizedModType
     {
+        public static bool state = false;
         public new string LocalizationCategory => "Items.Misc";
         public override void SetDefaults()
         {
@@ -21,9 +25,67 @@ namespace CalamityMod.Items
 			itemGroup = (ContentSamples.CreativeHelper.ItemGroup)CalamityResearchSorting.SpawnPrevention;
 		}
 
-        public override void UpdateInventory(Player player)
+        public override bool CanRightClick() => true;
+
+        public override void RightClick(Player player)
         {
-            player.Calamity().noStupidNaturalARSpawns = true;
+            if (player.Calamity().disableAnahitaSpawns == true)
+                player.Calamity().disableAnahitaSpawns = false;
+            else
+                player.Calamity().disableAnahitaSpawns = true;
+            state = player.Calamity().disableAnahitaSpawns;
+
+            bool favorited = Item.favorited;
+            Item.SetDefaults(ModContent.ItemType<BrokenWaterFilter>());
+            Item.stack++;
+            Item.favorited = favorited;
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frameI, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Texture2D texture;
+
+            if (state)
+            {
+                //Replace with enabled texture
+                texture = ModContent.Request<Texture2D>("CalamityMod/Items/BrokenWaterFilter").Value;
+                spriteBatch.Draw(texture, position, null, Color.White, 0f, origin, scale, SpriteEffects.None, 0);
+            }
+            else
+            {
+                texture = ModContent.Request<Texture2D>("CalamityMod/Items/BrokenWaterFilter").Value;
+                spriteBatch.Draw(texture, position, null, Color.White, 0f, origin, scale, SpriteEffects.None, 0);
+            }
+
+            return false;
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Texture2D texture;
+
+            if (state)
+            {
+                //Replace with enabled texture
+                texture = ModContent.Request<Texture2D>("CalamityMod/Items/BrokenWaterFilter").Value;
+                spriteBatch.Draw(texture, Item.position - Main.screenPosition, null, lightColor, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0);
+            }
+            else
+            {
+                texture = ModContent.Request<Texture2D>("CalamityMod/Items/BrokenWaterFilter").Value;
+                spriteBatch.Draw(texture, Item.position - Main.screenPosition, null, lightColor, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0);
+            }
+            return false;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            string text;
+            if (state == true)
+                text = "Currently On";
+            else
+                text = "Currently Off";
+            tooltips.FindAndReplace("[STATE]", text);
         }
 
         public override void AddRecipes()
