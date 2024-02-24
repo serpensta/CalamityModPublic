7b950f5098f30ddb2e793b457c9c6bfc9cc577d9
mochi-angel <snowyqueenofducks@gmail.com>
2024-02-24 03:33:10 -0800

    Scorpio Balance and Cluster resists

--- Projectiles/Ranged/ScorpioLargeRocket.cs
+++ Projectiles/Ranged/ScorpioLargeRocket.cs
@@ -110,9 +110,9 @@ namespace CalamityMod.Projectiles.Ranged
 
         public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
         {
-            modifiers.SourceDamage *= 13f;
+            modifiers.SourceDamage *= 18f;
             if (Projectile.numHits == 1)
-                Projectile.damage = (int)(Projectile.damage * 0.2f); // Inital shot does big damage, AOE does a 5th of that damage so Deus doesn't get to cash in for healthcare
+                Projectile.damage = (int)(Projectile.damage * 0.4f); // Inital shot does big damage, AOE does a 5th of that damage so Deus doesn't get to cash in for healthcare
             if (Projectile.numHits > 1)
                 Projectile.damage = (int)(Projectile.damage * 0.92f); // 8% penalty on explosion hits
             if (Projectile.damage < 1)
