aaba0dbad934d274244955ae8b8b75146177d7ea
GinYuH <72181395+GinYuH@users.noreply.github.com>
2024-02-24 08:07:18 -0800

    masterrev skeletron prime music

---
+++ Scenes/MusicScenes/SkeletronPrimeMasterMusicScene.cs
@@ -0,0 +1,14 @@
+﻿using CalamityMod.World;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Systems
+{
+    public class SkeletronPrimeMasterMusicScene : ModSceneEffect
+    {
+        public override int Music => MusicID.Boss3; // I spent 20 minutes trying to get this music to play while forgetting to actually override this in the first place _ YuH
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+        public override bool IsSceneEffectActive(Player player) => NPC.AnyNPCs(NPCID.SkeletronPrime) && Main.masterMode && CalamityWorld.revenge;
+    }
+}
