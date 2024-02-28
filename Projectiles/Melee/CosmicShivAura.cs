3525e8e291e1136bc2643ca18610ba4bcc3427f2
Ozzatron <spazeglitch@gmail.com>
2024-02-28 12:30:18 -0500

    silence localized thot

--- Projectiles/Melee/CosmicShivAura.cs
+++ Projectiles/Melee/CosmicShivAura.cs
@@ -1,11 +1,11 @@
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Projectiles.Melee
 {
-    public class CosmicShivAura : ModProjectile
+    public class CosmicShivAura : ModProjectile, ILocalizedModType
     {
         public new string LocalizationCategory => "Projectiles.Melee";
         public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
