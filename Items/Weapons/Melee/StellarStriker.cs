1ae7473eb64bdeae8180cd941cc47878d897221a
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-03-03 17:34:40 -0500

    Added a CalculatePredictiveAimToTarget util that accounts for projectile max updates
    *Reimplemented Mirror of Kalandra's bow extra updates using this function
    *Stellar Striker balance, changing lunar flares to aim at the target instead of the mouse, using the new CalculatePredictiveAimToTarget function
    *If there is anything else that could use this, feel free to implement it

--- Items/Weapons/Melee/StellarStriker.cs
+++ Items/Weapons/Melee/StellarStriker.cs
@@ -15,8 +15,8 @@ namespace CalamityMod.Items.Weapons.Melee
         {
             Item.width = 90;
             Item.height = 100;
-            Item.scale = 1.5f;
-            Item.damage = 480;
+            Item.scale = 1.6f;
+            Item.damage = 450;
             Item.DamageType = DamageClass.Melee;
             Item.useAnimation = 25;
             Item.useStyle = ItemUseStyleID.Swing;
@@ -36,62 +36,54 @@ namespace CalamityMod.Items.Weapons.Melee
         public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
         {
             if (player.whoAmI == Main.myPlayer)
-                SpawnFlares(player, Item.knockBack, GetOnHitDamage(player));
+                SpawnFlaresNPC(player, Item.knockBack, GetOnHitDamage(player), target);
         }
 
         public override void OnHitPvp(Player player, Player target, Player.HurtInfo hurtInfo)
         {
             if (player.whoAmI == Main.myPlayer)
-                SpawnFlares(player, Item.knockBack, GetOnHitDamage(player));
+                SpawnFlaresPvp(player, Item.knockBack, GetOnHitDamage(player), target);
         }
 
-        private void SpawnFlares(Player player, float knockback, int damage)
+        private void SpawnFlaresNPC(Player player, float knockback, int damage, NPC target)
         {
             var source = player.GetSource_ItemUse(Item);
             SoundEngine.PlaySound(SoundID.Item88, player.Center);
             int i = Main.myPlayer;
             float cometSpeed = Item.shootSpeed;
             Vector2 realPlayerPos = player.RotatedRelativePoint(player.MountedCenter, true);
-            float mouseXDist = (float)Main.mouseX + Main.screenPosition.X - realPlayerPos.X;
-            float mouseYDist = (float)Main.mouseY + Main.screenPosition.Y - realPlayerPos.Y;
-            if (player.gravDir == -1f)
-            {
-                mouseYDist = Main.screenPosition.Y + (float)Main.screenHeight - (float)Main.mouseY - realPlayerPos.Y;
-            }
-            float mouseDistance = (float)Math.Sqrt((double)(mouseXDist * mouseXDist + mouseYDist * mouseYDist));
-            if ((float.IsNaN(mouseXDist) && float.IsNaN(mouseYDist)) || (mouseXDist == 0f && mouseYDist == 0f))
-            {
-                mouseXDist = (float)player.direction;
-                mouseYDist = 0f;
-                mouseDistance = cometSpeed;
-            }
-            else
-            {
-                mouseDistance = cometSpeed / mouseDistance;
-            }
 
             for (int j = 0; j < 2; j++)
             {
                 realPlayerPos = new Vector2(player.Center.X + (float)(Main.rand.Next(201) * -(float)player.direction) + ((float)Main.mouseX + Main.screenPosition.X - player.position.X), player.MountedCenter.Y - 600f);
                 realPlayerPos.X = (realPlayerPos.X + player.Center.X) / 2f + (float)Main.rand.Next(-200, 201);
                 realPlayerPos.Y -= (float)(100 * j);
-                mouseXDist = (float)Main.mouseX + Main.screenPosition.X - realPlayerPos.X + (float)Main.rand.Next(-40, 41) * 0.03f;
-                mouseYDist = (float)Main.mouseY + Main.screenPosition.Y - realPlayerPos.Y;
-                if (mouseYDist < 0f)
-                {
-                    mouseYDist *= -1f;
-                }
-                if (mouseYDist < 20f)
-                {
-                    mouseYDist = 20f;
-                }
-                mouseDistance = (float)Math.Sqrt((double)(mouseXDist * mouseXDist + mouseYDist * mouseYDist));
-                mouseDistance = cometSpeed / mouseDistance;
-                mouseXDist *= mouseDistance;
-                mouseYDist *= mouseDistance;
-                float speedX = mouseXDist;
-                float speedY = mouseYDist + (float)Main.rand.Next(-80, 81) * 0.02f;
-                int proj = Projectile.NewProjectile(source, realPlayerPos.X, realPlayerPos.Y, speedX, speedY, ProjectileID.LunarFlare, damage, knockback, i, 0f, (float)Main.rand.Next(3));
+
+                Vector2 flareVelocity = CalamityUtils.CalculatePredictiveAimToTargetMaxUpdates(realPlayerPos, target, cometSpeed, 6);
+
+                int proj = Projectile.NewProjectile(source, realPlayerPos, flareVelocity, ProjectileID.LunarFlare, damage, knockback, i, 0f, (float)Main.rand.Next(3));
+                if (proj.WithinBounds(Main.maxProjectiles))
+                    Main.projectile[proj].DamageType = DamageClass.Melee;
+            }
+        }
+
+        private void SpawnFlaresPvp(Player player, float knockback, int damage, Player target)
+        {
+            var source = player.GetSource_ItemUse(Item);
+            SoundEngine.PlaySound(SoundID.Item88, player.Center);
+            int i = Main.myPlayer;
+            float cometSpeed = Item.shootSpeed;
+            Vector2 realPlayerPos = player.RotatedRelativePoint(player.MountedCenter, true);
+
+            for (int j = 0; j < 2; j++)
+            {
+                realPlayerPos = new Vector2(player.Center.X + (float)(Main.rand.Next(201) * -(float)player.direction) + ((float)Main.mouseX + Main.screenPosition.X - player.position.X), player.MountedCenter.Y - 600f);
+                realPlayerPos.X = (realPlayerPos.X + player.Center.X) / 2f + (float)Main.rand.Next(-200, 201);
+                realPlayerPos.Y -= (float)(100 * j);
+
+                Vector2 flareVelocity = Vector2.Normalize(target.Center - realPlayerPos) * cometSpeed;
+
+                int proj = Projectile.NewProjectile(source, realPlayerPos, flareVelocity, ProjectileID.LunarFlare, damage, knockback, i, 0f, (float)Main.rand.Next(3));
                 if (proj.WithinBounds(Main.maxProjectiles))
                     Main.projectile[proj].DamageType = DamageClass.Melee;
             }
