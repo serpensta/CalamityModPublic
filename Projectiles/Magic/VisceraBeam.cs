5e1310c17d45de8f9a870586f72d8dd325f179ce
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-03-19 17:07:08 -0400

    Viscera rework
    Plus Stellar Cannon balance

--- Projectiles/Magic/VisceraBeam.cs
+++ Projectiles/Magic/VisceraBeam.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CalamityMod.Items.Weapons.Magic;
+using CalamityMod.Particles;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -10,8 +12,6 @@ namespace CalamityMod.Projectiles.Magic
         public new string LocalizationCategory => "Projectiles.Magic";
         public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
 
-        public float healAmt = 1f;
-
         public override void SetDefaults()
         {
             Projectile.width = 4;
@@ -19,31 +19,22 @@ namespace CalamityMod.Projectiles.Magic
             Projectile.friendly = true;
             Projectile.DamageType = DamageClass.Magic;
             Projectile.ignoreWater = true;
-            Projectile.penetrate = 9;
-            Projectile.extraUpdates = 100;
+            Projectile.penetrate = 1;
+            Projectile.MaxUpdates = 100;
             Projectile.timeLeft = 300;
             Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 5;
+            Projectile.localNPCHitCooldown = 10;
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
-            healAmt *= 1.25f;
-            Projectile.penetrate--;
-            if (Projectile.penetrate <= 0)
+            if (Projectile.velocity.X != oldVelocity.X)
             {
-                Projectile.Kill();
+                Projectile.velocity.X = -oldVelocity.X;
             }
-            else
+            if (Projectile.velocity.Y != oldVelocity.Y)
             {
-                if (Projectile.velocity.X != oldVelocity.X)
-                {
-                    Projectile.velocity.X = -oldVelocity.X;
-                }
-                if (Projectile.velocity.Y != oldVelocity.Y)
-                {
-                    Projectile.velocity.Y = -oldVelocity.Y;
-                }
+                Projectile.velocity.Y = -oldVelocity.Y;
             }
             return false;
         }
@@ -53,16 +44,23 @@ namespace CalamityMod.Projectiles.Magic
             if (Main.player[Projectile.owner].moonLeech)
                 return;
 
-            healAmt *= 1.25f;
             Player player = Main.player[Projectile.owner];
-            player.statLife += (int)healAmt;
-            player.HealEffect((int)healAmt);
+            player.statLife += 1;
+            player.HealEffect(1);
+
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<VisceraBoom>(), (int)(Projectile.damage * 0.75f), Projectile.knockBack, Projectile.owner);
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            Particle bloodsplosion = new CustomPulse(Projectile.Center, Vector2.Zero, new Color(255, 32, 32), "CalamityMod/Particles/PlasmaExplosion", Vector2.One, Main.rand.NextFloat(-15f, 15f), 0.03f, 0.0875f, Viscera.BoomLifetime);
+            GeneralParticleHandler.SpawnParticle(bloodsplosion);
         }
 
         public override void AI()
         {
             Projectile.localAI[0] += 1f;
-            if (Projectile.localAI[0] > 9f)
+            if (Projectile.localAI[0] > 17f)
             {
                 for (int i = 0; i < 4; i++)
                 {
---
+++ Projectiles/Magic/VisceraBoom.cs
@@ -0,0 +1,38 @@
+﻿using CalamityMod.Buffs.DamageOverTime;
+using CalamityMod.Items.Weapons.Magic;
+using CalamityMod.Particles;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Magic
+{
+    public class VisceraBoom : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Magic";
+        public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 150;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = Viscera.BoomLifetime;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(ModContent.BuffType<BurningBlood>(), 90);
+        }
+
+        public override void OnHitPlayer(Player target, Player.HurtInfo info)
+        {
+            target.AddBuff(ModContent.BuffType<BurningBlood>(), 90);
+        }
+    }
+}
