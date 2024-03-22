6023208855c172079ec784d59e2c1cdbddbde297
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-03-22 11:14:52 -0400

    Provi resists Elemental Lance

--- Balancing/BalancingChangesManager.cs
+++ Balancing/BalancingChangesManager.cs
@@ -264,6 +264,9 @@ namespace CalamityMod.Balancing
             #region Providence
             // 80% resist to Hell's Sun.
             NPCSpecificBalancingChanges.Add(new NPCBalancingChange(NPCType<Providence>(), new ProjectileResistBalancingRule(0.2f, ProjectileType<HellsSunProj>())));
+
+            // 35% resist to Elemental Lance. This thing desperately needs a rework.
+            NPCSpecificBalancingChanges.Add(new NPCBalancingChange(NPCType<Providence>(), new ProjectileResistBalancingRule(0.65f, ProjectileType<SpatialSpear>(), ProjectileType<SpatialSpear2>(), ProjectileType<SpatialSpear3>(), ProjectileType<SpatialSpear4>())));
             #endregion
 
             #region Ceaseless Void: Dark Energies
