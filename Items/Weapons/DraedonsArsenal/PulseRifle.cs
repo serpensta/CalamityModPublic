82763b0761ce9f749ed074711f93301ac877ac95
Xyk <MetroidWhisperer@gmail.com>
2024-03-23 09:56:26 -0700

    Started The Rework

--- Items/Weapons/DraedonsArsenal/PulseRifle.cs
+++ Items/Weapons/DraedonsArsenal/PulseRifle.cs
@@ -19,7 +19,8 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
         public new string LocalizationCategory => "Items.Weapons.DraedonsArsenal";
         public static readonly SoundStyle FireSound = new("CalamityMod/Sounds/Item/PulseRifleFire");
 
-        private int BaseDamage = 1200;
+        private int BaseDamage = 2000;
+        public int FiringTimer = 30;
 
         public override void SetDefaults()
         {
@@ -30,12 +31,12 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
             Item.DamageType = DamageClass.Ranged;
             Item.damage = BaseDamage;
             Item.knockBack = 0f;
-            Item.useTime = 35;
-            Item.useAnimation = 35;
+            Item.useTime = 55;
+            Item.useAnimation = 55;
             Item.autoReuse = true;
 
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.UseSound = FireSound;
+            Item.UseSound = null;
             Item.noMelee = true;
 
             Item.value = CalamityGlobalItem.Rarity14BuyPrice;
@@ -51,16 +52,9 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            if (velocity.Length() > 5f)
-            {
-                velocity.Normalize();
-                velocity *= 5f;
-            }
+            SoundEngine.PlaySound(FireSound with { Pitch = 0.2f });
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<PulseRifleShot>(), damage, knockback, player.whoAmI, 0f, 0f);
 
-            float SpeedX = velocity.X + (float)Main.rand.Next(-1, 2) * 0.05f;
-            float SpeedY = velocity.Y + (float)Main.rand.Next(-1, 2) * 0.05f;
-
-            Projectile.NewProjectile(source, position, new Vector2(SpeedX, SpeedY), ModContent.ProjectileType<PulseRifleShot>(), damage, knockback, player.whoAmI, 0f, 0f);
             return false;
         }
 
