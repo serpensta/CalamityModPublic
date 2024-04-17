4f0602bd542fbb539b508f523b1b6a17451a0131
Memes <sorokinalex405@gmail.com>
2024-04-17 20:02:01 +0200

    Flak Toxicannon uses BaseGunHoldoutProjectile

--- Projectiles/Ranged/FlakToxicannonHoldout.cs
+++ Projectiles/Ranged/FlakToxicannonHoldout.cs
@@ -1,71 +1,46 @@
-﻿using System;
-using CalamityMod.Items.Weapons.Ranged;
-using CalamityMod.Particles;
+﻿using CalamityMod.Particles;
+using CalamityMod.Projectiles.BaseProjectiles;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.Audio;
-using Terraria.DataStructures;
 using Terraria.ID;
-using Terraria.Localization;
-using Terraria.ModLoader;
 using static CalamityMod.Items.Weapons.Ranged.FlakToxicannon;
 using static Terraria.ModLoader.ModContent;
 
 namespace CalamityMod.Projectiles.Ranged
 {
-    public class FlakToxicannonHoldout : ModProjectile, ILocalizedModType
+    public class FlakToxicannonHoldout : BaseGunHoldoutProjectile
     {
-        public override LocalizedText DisplayName => CalamityUtils.GetItemName<FlakToxicannon>();
-        public override string Texture => "CalamityMod/Items/Weapons/Ranged/FlakToxicannon";
+        public override float MaxOffsetLengthFromArm => 38f;
+        public override float RecoilResolveSpeed => 0.1f;
+        public override float OffsetXUpwards => -10f;
+        public override float OffsetXDownwards => 2f;
+        public override float BaseOffsetY => -10f;
+        public override float OffsetYUpwards => 10f;
+        public override float OffsetYDownwards => 5f;
 
-        public Player Owner { get; set; }
         public ref float ShootingTimer => ref Projectile.ai[0];
         public ref float TimerBetweenBursts => ref Projectile.ai[1];
-        public ref float OffsetLengthScalar => ref Projectile.localAI[0];
 
         // The type of dust used in the effects and the color of the particles that it'll use depending on the type of rocket used.
         // It'll carry over to the projectiles that it shoots.
         public static int DustEffectsID { get; set; }
         public static Color EffectsColor { get; set; }
 
-        public override void SetDefaults()
+        public override void HoldoutAI()
         {
-            Projectile.width = Projectile.height = 88;
-            Projectile.tileCollide = false;
-            Projectile.ignoreWater = true;
-            Projectile.netImportant = true;
-        }
-
-        public override void AI()
-        {
-            // If there's no player, or the player is the server, or the owner's stunned, there'll be no holdout.
-            if (Owner.CantUseHoldout())
-            {
-                Projectile.Kill();
-                NetUpdate();
-                return;
-            }
-
             // The center of the player, taking into account if they have a mount or not.
             Vector2 mountedCenter = Owner.MountedCenter;
 
             // The vector between the player and the mouse.
             Vector2 ownerToMouse = Owner.Calamity().mouseWorld - mountedCenter;
 
-            // Deals with the holdout's rotation and direction, the owner's arms, etc.
-            ManageHoldout(mountedCenter, ownerToMouse);
-
             // If the timer reaches Item.useTime, it'll shoot.
             // It'll shoot once immediately as the timer reaches.
-            Item heldItem = Owner.ActiveItem();
-            if (ShootingTimer >= heldItem.useAnimation)
+            if (ShootingTimer >= HeldItem.useAnimation)
             {
                 // We use the velocity of this projectile as its direction vector.
                 Vector2 direction = Projectile.velocity.SafeNormalize(Vector2.Zero);
-
-                // The position of the tip of the gun.
-                Vector2 nuzzlePosition = Projectile.Center + direction * Projectile.width / 2f;
 
                 // Every time we shoot we use ammo.
                 // With this method we also use the item's stats, like the shoot speed, or the type of ammo it was used.
@@ -95,7 +70,7 @@ namespace CalamityMod.Projectiles.Ranged
                 // Spawns the projectile.
                 Projectile.NewProjectileDirect(
                     Projectile.GetSource_FromThis(),
-                    nuzzlePosition,
+                    GunTipPosition,
                     direction * itemShootSpeed,
                     ProjectileType<FlakToxicannonProjectile>(),
                     itemDamage,
@@ -112,11 +87,11 @@ namespace CalamityMod.Projectiles.Ranged
                 if (!Main.dedServ)
                 {
                     // By decreasing the offset length of the gun from the arms, we give an effect of recoil.
-                    OffsetLengthScalar = 25f;
+                    OffsetLengthFromArm = 25f;
 
                     Owner.Calamity().GeneralScreenShakePower = 2f;
 
-                    Particle shootPulse = new DirectionalPulseRing(nuzzlePosition,
+                    Particle shootPulse = new DirectionalPulseRing(GunTipPosition,
                         Vector2.Zero,
                         Color.Gray * 0.7f,
                         new Vector2(0.5f, 1f),
@@ -130,7 +105,7 @@ namespace CalamityMod.Projectiles.Ranged
                     for (int i = 0; i < smokeAmount; i++)
                     {
                         Particle smoke = new HeavySmokeParticle(
-                            nuzzlePosition,
+                            GunTipPosition,
                             direction.RotatedByRandom(MathHelper.ToRadians(25f)) * Main.rand.NextFloat(2f, 30f),
                             EffectsColor * 0.4f,
                             Main.rand.Next(45, 61),
@@ -145,7 +120,7 @@ namespace CalamityMod.Projectiles.Ranged
                     for (int i = 0; i < dustAmount; i++)
                     {
                         Dust shootDust = Dust.NewDustPerfect(
-                            nuzzlePosition,
+                            GunTipPosition,
                             DustEffectsID,
                             direction.RotatedByRandom(MathHelper.PiOver4 * 0.5f) * Main.rand.NextFloat(2f, 12f),
                             Scale: Main.rand.NextFloat(0.8f, 1f));
@@ -154,83 +129,14 @@ namespace CalamityMod.Projectiles.Ranged
                         shootDust.noLightEmittence = false;
                     }
 
-                    SoundEngine.PlaySound(new SoundStyle("CalamityMod/Sounds/Item/FlakKrakenShoot") { Pitch = 0.65f, Volume = 0.5f }, nuzzlePosition);
+                    SoundEngine.PlaySound(new SoundStyle("CalamityMod/Sounds/Item/FlakKrakenShoot") { Pitch = 0.65f, Volume = 0.5f }, GunTipPosition);
                 }
 
                 ShootingTimer = 0f;
-
-                NetUpdate();
+                Projectile.netUpdate = true;
             }
 
-            // When we change the distance of the gun from the arms for the recoil,
-            // recover to the original position smoothly.
-            if (OffsetLengthScalar != 38f)
-                OffsetLengthScalar = MathHelper.Lerp(OffsetLengthScalar, 38f, 0.1f);
-
             ShootingTimer++;
-        }
-
-        #region AI Methods
-
-        public void ManageHoldout(Vector2 mountedCenter, Vector2 ownerToMouse)
-        {
-            Vector2 rotationVector = Projectile.rotation.ToRotationVector2();
-            float velocityRotation = Projectile.velocity.ToRotation();
-            float proximityLookingUpwards = Vector2.Dot(ownerToMouse.SafeNormalize(Vector2.Zero), -Vector2.UnitY);
-            int direction = MathF.Sign(ownerToMouse.X);
-
-            Vector2 armPosition = Owner.RotatedRelativePoint(mountedCenter, true);
-            Vector2 lengthOffset = rotationVector * OffsetLengthScalar;
-            Vector2 armOffset = new Vector2(Utils.Remap(proximityLookingUpwards, -1f, 1f, 2f, -10f) * direction, -10f + Utils.Remap(MathF.Abs(proximityLookingUpwards), 0f, 1f, 0f, proximityLookingUpwards > 0f ? 10f : 5f));
-            Projectile.Center = armPosition + lengthOffset + armOffset;
-            Projectile.velocity = velocityRotation.AngleTowards(ownerToMouse.ToRotation(), 0.2f).ToRotationVector2();
-            Projectile.rotation = velocityRotation;
-            Projectile.timeLeft = 2;
-
-            Owner.heldProj = Projectile.whoAmI;
-            Owner.itemTime = Owner.itemAnimation = 2;
-            Owner.itemRotation = (Projectile.velocity * Projectile.direction).ToRotation();
-
-            Projectile.spriteDirection = Projectile.direction = direction;
-            Owner.ChangeDir(direction);
-
-            float armRotation = Projectile.rotation - MathHelper.PiOver2; // -Pi/2 because the arms rotation starts with arms pointing down.
-            Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.ThreeQuarters, armRotation);
-            Owner.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, armRotation + MathHelper.ToRadians(25f) * direction);
-        }
-
-        public void NetUpdate()
-        {
-            Projectile.netUpdate = true;
-            if (Projectile.netSpam >= 10)
-                Projectile.netSpam = 9;
-        }
-
-        #endregion
-
-        public override void OnSpawn(IEntitySource source)
-        {
-            Owner = Main.player[Projectile.owner];
-            OffsetLengthScalar = 38f;
-        }
-
-        // Because we use the velocity as a direction, we don't need it to change its position.
-        public override bool ShouldUpdatePosition() => false;
-
-        public override bool? CanDamage() => false;
-
-        public override bool PreDraw(ref Color lightColor)
-        {
-            Texture2D texture = Request<Texture2D>(Texture).Value;
-            Vector2 drawPosition = Projectile.Center - Main.screenPosition;
-            Color drawColor = Projectile.GetAlpha(lightColor);
-            float drawRotation = Projectile.rotation + (Projectile.spriteDirection == -1 ? MathHelper.Pi : 0f);
-            Vector2 rotationPoint = texture.Size() * 0.5f;
-            SpriteEffects flipSprite = Projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
-
-            Main.EntitySpriteDraw(texture, drawPosition, null, drawColor, drawRotation, rotationPoint, Projectile.scale, flipSprite);
-
-            return false;
         }
     }
 }
