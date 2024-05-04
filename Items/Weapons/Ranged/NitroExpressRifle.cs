7013ee54278efdc3f8e32ed1fb18460f39047ac2
Xyk <MetroidWhisperer@gmail.com>
2024-05-03 19:58:51 -0700

    Nitro Express Rifle
    Adds Nitro Express Rifle

---
+++ Items/Weapons/Ranged/NitroExpressRifle.cs
@@ -0,0 +1,62 @@
+﻿using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles.Ranged;
+using CalamityMod.Rarities;
+using CalamityMod.Sounds;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Ranged
+{
+    public class NitroExpressRifle : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Weapons.Ranged";
+        public static readonly SoundStyle FireSound = new("CalamityMod/Sounds/Item/NitroExpressRifleFire") { Volume = 0.6f };
+        public override void SetDefaults()
+        {
+            Item.width = 100;
+            Item.height = 22;
+            Item.damage = 2400;
+            Item.DamageType = DamageClass.Ranged;
+            Item.useTime = 80;
+            Item.useAnimation = 80;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 8.5f;
+            Item.value = CalamityGlobalItem.RarityPureGreenBuyPrice;
+            Item.UseSound = FireSound;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<NitroShot>();
+            Item.shootSpeed = 12f;
+            Item.useAmmo = AmmoID.Bullet;
+            Item.rare = ModContent.RarityType<PureGreen>();
+            Item.Calamity().canFirePointBlankShots = true;
+        }
+
+        public override Vector2? HoldoutOffset() => new Vector2(-30, 0);
+
+        // Terraria seems to really dislike high crit values in SetDefaults
+        public override void ModifyWeaponCrit(Player player, ref float crit) => crit += 10;
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.SniperRifle).
+                AddIngredient<GalacticaSingularity>(3).
+                AddIngredient<DarkPlasma>(2).
+                AddTile(TileID.LunarCraftingStation).
+                Register();
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            player.velocity += -velocity * 0.65f;
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<NitroShot>(), damage, knockback, player.whoAmI, 0f);
+            return false;
+        }
+    }
+}
