c03f3a22da9e9232f199b0a72a8ce65662458c5d
Xyk <MetroidWhisperer@gmail.com>
2024-05-12 14:00:27 -0700

    Nitro tiershift + fixes
    Also made rockets pre mech so you can use flak toxicannon if you fight AS first
    Added "uses rockets" to the early rocket launchers

--- Items/Weapons/Ranged/NitroExpressRifle.cs
+++ Items/Weapons/Ranged/NitroExpressRifle.cs
@@ -22,13 +22,13 @@ namespace CalamityMod.Items.Weapons.Ranged
         {
             Item.width = 100;
             Item.height = 22;
-            Item.damage = 1350;
+            Item.damage = 100;
             Item.DamageType = DamageClass.Ranged;
-            Item.useTime = 80;
-            Item.useAnimation = 80;
+            Item.useTime = 70;
+            Item.useAnimation = 70;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
-            Item.knockBack = 8.5f;
+            Item.knockBack = 7.5f;
             Item.value = CalamityGlobalItem.RarityPureGreenBuyPrice;
             Item.UseSound = FireSound;
             Item.autoReuse = true;
@@ -47,16 +47,25 @@ namespace CalamityMod.Items.Weapons.Ranged
         public override void AddRecipes()
         {
             CreateRecipe().
-                AddIngredient(ItemID.SniperRifle).
-                AddIngredient<GalacticaSingularity>(3).
-                AddIngredient<DarkPlasma>(2).
-                AddTile(TileID.LunarCraftingStation).
+                AddIngredient(ItemID.Musket).
+                AddIngredient<EssenceofHavoc>(3).
+                AddIngredient<EssenceofSunlight>(3).
+                AddIngredient(ItemID.ExplosivePowder, 5).
+                AddTile(TileID.Anvils).
+                Register();
+
+            CreateRecipe().
+                AddIngredient(ItemID.TheUndertaker).
+                AddIngredient<EssenceofHavoc>(3).
+                AddIngredient<EssenceofSunlight>(3).
+                AddIngredient(ItemID.ExplosivePowder, 5).
+                AddTile(TileID.Anvils).
                 Register();
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            player.velocity += -velocity * 0.45f;
+            player.velocity += -velocity * 0.3f;
             Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<NitroShot>(), damage, knockback, player.whoAmI, 0f);
             return false;
         }
