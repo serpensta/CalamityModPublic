9eb8ed56d2ca124d287444940b048d158786d48f
mochi-angel <snowyqueenofducks@gmail.com>
2024-05-13 00:56:13 -0700

    Forgot Genesis

--- Items/Weapons/Magic/Genesis.cs
+++ Items/Weapons/Magic/Genesis.cs
@@ -14,7 +14,7 @@ namespace CalamityMod.Items.Weapons.Magic
         public new string LocalizationCategory => "Items.Weapons.Magic";
         public override void SetDefaults()
         {
-            Item.width = 108;
+            Item.width = 116;
             Item.height = 46;
             Item.damage = 60;
             Item.DamageType = DamageClass.Magic;
