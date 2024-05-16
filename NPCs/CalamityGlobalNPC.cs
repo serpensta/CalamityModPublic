c90bedb1d533f219ceb87314b0507238fab34e55
mountaindrew10 <marhaj04@gmail.com>
2024-05-15 19:03:42 -0500

    Destroyer mp bug fixes.

--- NPCs/CalamityGlobalNPC.cs
+++ NPCs/CalamityGlobalNPC.cs
@@ -6950,13 +6950,13 @@ namespace CalamityMod.NPCs
                 if (npc.spriteDirection == 1)
                     spriteEffects = SpriteEffects.FlipHorizontally;
 
-                if (npc.type == NPCID.TheDestroyerBody)
+                /*if (npc.type == NPCID.TheDestroyerBody)
                 {
                     if (npc.ai[2] == 0f)
                         npc.frame.Y = 0;
                     else
                         npc.frame.Y = frameHeight;
-                }
+                }*/
 
                 Color segmentDrawColor = npc.GetAlpha(drawColor);
 
