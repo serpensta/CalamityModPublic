53d67c8081654d0a1285abf18e4c764028204514
Ozzatron <spazeglitch@gmail.com>
2024-05-17 16:25:44 -0400

    1 - Stopped Terratomere from drawing its trails multiple times in an attempt to stop projectiles from flickering out of existence. 2 - Renamed Terratomere projectiles to make more sense.

--- Projectiles/Melee/TerratomereHoldoutProj.cs
+++ Projectiles/Melee/TerratomereHoldoutProj.cs
@@ -169,21 +169,21 @@ namespace CalamityMod.Projectiles.Melee
                 for (int i = 0; i < totalBeams; i++)
                 {
                     Vector2 randomVelocity = Main.rand.NextVector2CircularEdge(randomVelocityLimit, randomVelocityLimit);
-                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center - bigSlashVelocity * 0.4f, bigSlashVelocity + randomVelocity, ModContent.ProjectileType<TerratomereBigSlash>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center - bigSlashVelocity * 0.4f, bigSlashVelocity + randomVelocity, ModContent.ProjectileType<TerratomereSwordBeam>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
                 }
             }
 
-            // This is a slash, not a beam.
+            // Close range slash effect
             if (Main.myPlayer == Projectile.owner && Time == (int)(Terratomere.SwingTime * RecoveryCompletionRatio) + 5f)
             {
                 Vector2 bigSlashVelocity = InitialRotation.ToRotationVector2() * Owner.ActiveItem().shootSpeed / 6f;
                 Vector2 bigSlashSpawnPosition = Projectile.Center + bigSlashVelocity.SafeNormalize(Vector2.UnitY) * 64f;
 
-                int slash = Projectile.NewProjectile(Projectile.GetSource_FromThis(), bigSlashSpawnPosition, bigSlashVelocity, ModContent.ProjectileType<TerratomereBeam>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                int slash = Projectile.NewProjectile(Projectile.GetSource_FromThis(), bigSlashSpawnPosition, bigSlashVelocity, ModContent.ProjectileType<TerratomereMeleeSlash>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
                 if (Main.projectile.IndexInRange(slash))
                 {
                     Main.projectile[slash].ai[0] = (Direction == 1f).ToInt();
-                    Main.projectile[slash].ModProjectile<TerratomereBeam>().ControlPoints = GenerateSlashPoints().ToArray();
+                    Main.projectile[slash].ModProjectile<TerratomereMeleeSlash>().ControlPoints = GenerateSlashPoints().ToArray();
                 }
             }
         }
