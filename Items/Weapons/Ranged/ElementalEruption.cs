6c9c9d3d5638527bbae7828d1cd15ce42c4c4c89
Memes <sorokinalex405@gmail.com>
2024-05-17 15:17:46 +0200

    Fixed Elemental Eruption in multiplayer.

--- Items/Weapons/Ranged/ElementalEruption.cs
+++ Items/Weapons/Ranged/ElementalEruption.cs
@@ -14,10 +14,7 @@ namespace CalamityMod.Items.Weapons.Ranged
 
         public int FlareCounter = 0;
 
-        public override void SetStaticDefaults()
-        {
-            ItemID.Sets.ItemsThatAllowRepeatedRightClick[Item.type] = true;
-        }
+        public override void SetStaticDefaults() => ItemID.Sets.ItemsThatAllowRepeatedRightClick[Type] = true;
 
         public override void SetDefaults()
         {
@@ -41,13 +38,18 @@ namespace CalamityMod.Items.Weapons.Ranged
         }
 
         public override Vector2? HoldoutOffset() => new Vector2(-10, 0);
+
         public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0;
+
+        public override void HoldItem(Player player) => player.Calamity().mouseWorldListener = true;
 
         // Spawning the holdout cannot consume ammo
         public override bool CanConsumeAmmo(Item ammo, Player player) => player.ownedProjectileCounts[Item.shoot] > 0;
+
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Projectile.NewProjectile(source, position, Vector2.Zero, type, 0, 0f, player.whoAmI);
+            Projectile holdout = Projectile.NewProjectileDirect(source, player.MountedCenter, Vector2.Zero, ModContent.ProjectileType<ElementalEruptionHoldout>(), damage, knockback, player.whoAmI, 0, 1);
+            holdout.velocity = player.Calamity().mouseWorld - player.RotatedRelativePoint(player.MountedCenter);
             return false;
         }
 
